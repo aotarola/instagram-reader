86137ee4224b2cf8bc046416a54878eca0dae0ba
Andres Otarola <andres@otarola.me>
2015-10-24 17:29:37 -0700

    Added basic app functionality

---
+++ app/src/main/java/me/otarola/instagramreader/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package me.otarola.instagramreader;
+
+/**
+ * Created by aotarolaalvarad on 10/24/15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
